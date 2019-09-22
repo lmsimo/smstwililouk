8461e952e1c3874f5bbb9005220cd71ee171fc2b
lmsimo <55662836+lmsimo@users.noreply.github.com>
2019-09-22 20:59:47 +0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -2,7 +2,7 @@ from twilio.rest import TwilioRestClient
 from credentials import account_sid, auth_token, my_cell, my_twilio
 
 # Find these values at https://twilio.com/user/account
-client = TwilioRestClient(account_sid, auth_token)
+client = TwilioRestClient(ACb021cf2144a7dbc92a717abcb315f4f7, 32fc5d5d9dba1eadd8809d56363429d5)
 
 my_msg = "hey ipran service"
 
